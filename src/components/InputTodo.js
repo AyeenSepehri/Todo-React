a85f0eae05e25edec197114b32802d57c5d37f2f
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-06-25 19:59:18 +0430

    working on validation of input of todo

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,8 +1,20 @@
 import React, {useState} from 'react'
 import TodoItem from './TodoItem'
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+  todo :yup.string().required("پر کردن لیست اجباری است")
+});
+
 function InputTodo() {
     const [todoValue , setTodoValue] = useState("")
     const [todoItm , setTodoItm] = useState([])
+
+    const { register, handleSubmit, formState: { errors }, reset } = useForm({
+      resolver: yupResolver(schema),
+    });
 
     const changeTodo = (e) => {
         setTodoValue(e.target.value)
@@ -12,7 +24,9 @@ function InputTodo() {
         const updateTodo = [...todoItm]
         updateTodo.push(todoValue)
         setTodoItm(updateTodo)
-        setTodoValue("")       
+        setTodoValue("")
+        reset()   
+        console.log("hiiiiii")
       }
 
       const deleteHandler = (numberTodo) => {
@@ -23,15 +37,19 @@ function InputTodo() {
         console.log(updateTodo)
         console.log(numberTodo)
       }
-      
+      const todoInputField = register("todo", { required: true });
   return (
     <div>
       <div>
         <label>insert your todos of today</label>
-        <input type='text' name='settodo' value={todoValue} onChange={changeTodo}/>
-        <button onClick={submitTodo} >add todo</button>
+        <input type='text' name='settodo' value={todoValue} {...todoInputField} 
+        onChange={(e) => {
+          todoInputField.onChange(e);
+          changeTodo(e)
+        }}/>
+        <button onClick={handleSubmit(submitTodo)} >add todo</button>
       </div> 
-
+      <p>{errors.todo?.message}</p>
       {todoItm.map((todo, index)=>{
         return <TodoItem 
         deleteItm={deleteHandler} todo={todo} numberTodo={index} key={index}
