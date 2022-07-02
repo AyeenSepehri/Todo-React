c7cd997b21955ee613f3ec2ce7d6dd61e5b5f333
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2022-07-02 20:22:59 +0430

    change markHandler function to a props function

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,18 +1,9 @@
-import React,{useState} from 'react'
-// import classes from "./TodoItem.module.css"
+import React from 'react'
 
 function TodoItem(props) {
 
-  const [complete , setComplete] = useState(false)
-
   const markHandler = () =>{
-    if(complete === false){
-      setComplete(true)
-      props.trueState(props.complete)
-    }
-    if(complete === true){
-      setComplete(false)
-    }
+    props.completeStatus(props.complete)
   }
 
   const deleteHandler = () => {
@@ -22,7 +13,7 @@ function TodoItem(props) {
   return (
     <div className='bg-white flex flex-row my-7 p-3'>
       <div className='my-auto'>
-        <p className={complete ? "complete line-through" : "uncoplete"} >{props.todo}</p>
+        <p className={props.complete ? "line-through" : ""} >{props.todo}</p>
       </div>
       <div className='mr-auto'>
         <button className ="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-1 rounded" onClick={markHandler}>mark</button>
